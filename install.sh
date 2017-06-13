8b60dc9c13ea1c5f9e0d621643e6c853e70e35a7
wderezin <walter.derezinski.contractor@weather.com>
2017-06-13 08:21:21 -0400

    updated RI reporting and installer

--- install.sh
+++ install.sh
@@ -12,8 +12,28 @@ cd $DIR || error "can not change to $DIR"
 
 # Extract to current directory
 curl -L https://api.github.com/repos/daringway/dare-aws-cli/tarball/master | gunzip -c | tar --strip-components=1 -xvf -
-
 chmod +x ./bin/*
 
-$DIR/bin/dare-aws-cli-setup
+# Cleanup old files
+rm ./bin/aws-report-ec2-ri-unused 2>/dev/null
+rm ./bin/dare-aws-cli-setup 2>/dev/null
 
+PROFILE=~/.bash_profile
+if [ -f ~/.profile ]
+then
+    PROFILE=~/.profile
+fi
+DENV=$(dirname $(dirname $0))/etc/dare-aws-cli.rc
+
+if ! grep $DENV $PROFILE >/dev/null 2>&1
+then
+    echo "" >> $PROFILE
+    echo "# ADDED by ${USER} on $(date)" >> $PROFILE
+    echo "# BY running $0" >> $PROFILE
+    echo ". $DENV" >> $PROFILE
+
+    echo "Added $DENV to your $PROFILE"
+    echo "Restart your login sesison and your are all set."
+else
+    echo "Your environment is already setup, you're all set."
+fi
